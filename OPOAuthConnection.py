f52b2cf60836d7e5d4e65b46f67a3e0c47d7283a
Lyle Hayhurst <sozinsky@gmail.com>
2011-02-27 21:28:46 -0600

    added error codes.

--- OPOAuthConnection.py
+++ OPOAuthConnection.py
@@ -13,6 +13,8 @@ class OPOAuthConnection:
         self.token = oauth.Token( config.get_oauth_token(), config.get_oauth_secret() )
         self.client = oauth.Client( self.consumer, self.token )
 
+        self._init_error_codes()
+
     def get(self, url, optional_params = {} ):
 
         params = {
@@ -36,10 +38,28 @@ class OPOAuthConnection:
         req.sign_request(signature_method, self.consumer, self.token)
         resp, content = self.client.request( req.to_url() )
 
-        if resp['status'] != '200':
-            raise Exception("Unable to fetch user information, reason: %s." % resp['status'])
+        status = resp['status']
+        if status != '200':
+            raise Exception("Unable to fetch user information, reason: %s, notes %s: " %( resp['status'],  self.error_codes[status]))
 
         return content
+
+    def _init_error_codes(self):
+        self.error_codes = {
+              '200' :	"Success. This is the most common response when everything works normally.",
+              '201'	:   "Created. This is returned when something is created, like a new wiki page.",
+              '204' :	"No Content. The server has received the request and acted on it, but nothing needs to be returned. " +
+                        "Often returned from a delete request.",
+              '400' :	"Request Error. The most common error response. Something was wrong with the request. " +
+                        "Inspect the error messages for more information.",
+              '403' :	"Forbidden. You are not authorized to see the resource. Often returned when requesting a private campaign " +
+                        "or a gm-only page. Inspect the error message for more information.",
+              '404' :	"Not Found. The resource you requested cannot be found.",
+              '500' :	"Server Error. An error happened on the server. If it continues, please notify us and we'll look into it.",
+              '503'	:   "Service Unavailable. The server is temporarily down for maintenance. We do this quite often, so we highly suggest " +
+                        "building your apps to be prepared for it. Note: No guarantee that the response body will be a valid format (ie." +
+                        " JSON or XML) so it's best to look for the 503 and stop parsing if it's found."
+            }
 
 """
 The MIT License
