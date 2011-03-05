842e57b78ed23acf10f8547b00efb7ff3f21172a
Lyle Hayhurst <sozinsky@gmail.com>
2011-03-05 09:28:16 -0600

    added more wiki tests

--- WikiAPI.py
+++ WikiAPI.py
@@ -30,15 +30,33 @@ class TestAPIWiki( unittest.TestCase ):
         wiki = self.wiki.get( oppyCmpnId )
         self.assertTrue( wiki )
         self.assertEqual( 3, len( wiki.pages() ))
-        page0 = wiki.pages()[0]
-        self.assertTrue( page0 )
-        self.assertTrue( page0.campaign() )
-        self.assertEqual( page0.campaign().id(), oppyCmpnId)
-        self.assertEqual( 'Home Page', page0.name() )
-        self.assertEqual( 'afa0074c42a111e0bbb240403656340d', page0.id() )
-        self.assertEqual( 'home-page', page0.slug() )
-        self.assertEqual( 'WikiPage', page0.page_type() )
-        self.assertEqual( 'http://www.obsidianportal.com/campaigns/oppy/wiki_pages/home-page', page0.wiki_page_url() )
+        homepage = wiki.pages()[0]
+        self.assertTrue( homepage )
+        self.assertTrue( homepage.campaign() )
+        self.assertEqual( homepage.campaign().id(), oppyCmpnId)
+        self.assertEqual( 'Home Page', homepage.name() )
+        self.assertEqual( 'afa0074c42a111e0bbb240403656340d', homepage.id() )
+        self.assertEqual( 'home-page', homepage.slug() )
+        self.assertEqual( 'WikiPage', homepage.page_type() )
+        self.assertEqual( 'http://www.obsidianportal.com/campaigns/oppy/wiki_pages/home-page', homepage.wiki_page_url() )
+        mainpage = wiki.pages()[1]
+        self.assertTrue( mainpage )
+        self.assertTrue( mainpage.campaign() )
+        self.assertEqual( mainpage.campaign().id(), oppyCmpnId)
+        self.assertEqual( 'Main Page', mainpage.name() )
+        self.assertEqual( 'b0437bb642a111e0bbb240403656340d', mainpage.id() )
+        self.assertEqual( 'main-page', mainpage.slug() )
+        self.assertEqual( 'WikiPage', mainpage.page_type() )
+        self.assertEqual( 'http://www.obsidianportal.com/campaigns/oppy/wiki_pages/main-page', mainpage.wiki_page_url() )
+        welcomepage = wiki.pages()[2]
+        self.assertTrue( welcomepage )
+        self.assertTrue( welcomepage.campaign() )
+        self.assertEqual( welcomepage.campaign().id(), oppyCmpnId)
+        self.assertEqual( 'Welcome', welcomepage.name() )
+        self.assertEqual( 'b076352442a111e0bbb240403656340d', welcomepage.id() )
+        self.assertEqual( 'welcome', welcomepage.slug() )
+        self.assertEqual( 'Post', welcomepage.page_type() )
+        self.assertEqual( 'http://www.obsidianportal.com/campaigns/oppy/wiki_pages/welcome', welcomepage.wiki_page_url() )
 
 
 
